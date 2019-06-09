66d8f1bbb476b9762d543d2d3a6e730c28678bf7
Leonid Goryaynov <lbg9992@mail.ru>
2019-06-09 15:30:43 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace RegistrationClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(135, 13);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Location = new System.Drawing.Point(135, 39);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.surnameTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surname";
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(135, 65);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(258, 114);
+            this.descriptionTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description";
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(108, 234);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(75, 23);
+            this.registerButton.TabIndex = 6;
+            this.registerButton.Text = "Register new user";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Enabled = false;
+            this.filePathTextBox.Location = new System.Drawing.Point(135, 185);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(177, 20);
+            this.filePathTextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Photo";
+            // 
+            // fileButton
+            // 
+            this.fileButton.Location = new System.Drawing.Point(318, 183);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(75, 23);
+            this.fileButton.TabIndex = 9;
+            this.fileButton.Text = "Choose file";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.FileButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 284);
+            this.Controls.Add(this.fileButton);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace RegistrationClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            filePathTextBox.Text = openFileDialog1.FileName;
+        }
+
+        private async void RegisterButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(nameTextBox.Text) || string.IsNullOrEmpty(surnameTextBox.Text) ||
+                string.IsNullOrEmpty(descriptionTextBox.Text) || string.IsNullOrEmpty(filePathTextBox.Text))
+            {
+                MessageBox.Show("Введены не все данные");
+                return;
+            }
+
+            MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;database=pictureusers;user=root;password=Root1234");
+
+            connection.Open();
+
+            MySqlCommand selectCurIndex = new MySqlCommand("select max(id) from users", connection);
+            string select = selectCurIndex.ExecuteScalar().ToString();
+
+            int curIndex;
+            if (String.IsNullOrEmpty(select))
+            {
+                curIndex = 1;
+            }
+            else
+            {
+                curIndex = Convert.ToInt32(select) + 1;
+            }
+
+            MySqlCommand insertNewUser = new MySqlCommand($"insert into users (id, name, surname, description) values ({curIndex}, '{nameTextBox.Text}'," +
+                $" '{surnameTextBox.Text}', '{descriptionTextBox.Text}')", connection);
+            insertNewUser.ExecuteNonQuery();
+
+            HttpClient httpClient = new HttpClient();
+            MultipartFormDataContent form = new MultipartFormDataContent();
+
+            form.Add(new StringContent("newUser"), "type");
+            form.Add(new StringContent(curIndex.ToString()), "index");
+
+            var file = File.ReadAllBytes(openFileDialog1.FileName);
+
+            form.Add(new ByteArrayContent(file, 0, file.Length), "image", openFileDialog1.FileName.Split('\\').Last());
+            HttpResponseMessage response = await httpClient.PostAsync(new UriBuilder("192.168.0.89:9999").Uri, form);
+
+            response.EnsureSuccessStatusCode();
+            httpClient.Dispose();
+            var sd = response.StatusCode;
+
+            if (sd == System.Net.HttpStatusCode.OK)
+            {
+                MessageBox.Show("New user registrated");
+                nameTextBox.Text = "";
+                surnameTextBox.Text = "";
+                descriptionTextBox.Text = "";
+                filePathTextBox.Text = "";
+                openFileDialog1.FileName = "";
+            }
+
+        }
+    }
+}
